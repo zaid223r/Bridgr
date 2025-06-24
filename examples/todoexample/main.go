1e8e01be5f2cb892906ae4062d8c7b439a5e0d0d
Zaid Mac <zaidzuhairalani@gmail.com>
2025-06-24 14:50:44 +0300

    chore(example): use models package for Todo and Note

--- examples/todoexample/main.go
+++ examples/todoexample/main.go
@@ -4,39 +4,20 @@ import (
 	brcrud "bridgr/core/brcrud"
 	brdb "bridgr/core/brdb"
 	brhttp "bridgr/core/brhttp"
+	"bridgr/examples/todoexample/models"
 )
-
-type Todo struct {
-	ID    uint   `json:"id" gorm:"primaryKey"`
-	Title string `json:"title"`
-	Done  bool   `json:"done"`
-}
-
-func (Todo) GenericFiltering() bool {
-	return true
-}
-
-type Note struct {
-	ID    uint   `json:"id" gorm:"primaryKey"`
-	Title string `json:"title"`
-	Body  string `json:"body"`
-}
-
-func (Note) FilterableFields() []string {
-	return []string{"title"}
-}
 
 func main() {
 	db, err := brdb.ConnectPostgres("test", "123456", "localhost", "bridgr", 5432)
 	if err != nil {
 		panic(err)
 	}
-	_ = db.AutoMigrate(&Todo{}, &Note{})
+	_ = db.AutoMigrate(&models.Todo{}, &models.Note{})
 
 	router := brhttp.NewRouter()
 
-	brcrud.RegisterCRUD[Todo](router, "todos", db, nil)
-	brcrud.RegisterCRUD[Note](router, "notes", db, nil)
+	brcrud.RegisterCRUD[models.Todo](router, "todos", db, nil)
+	brcrud.RegisterCRUD[models.Note](router, "notes", db, nil)
 
 	brhttp.StartServer(router, "8080")
 }
